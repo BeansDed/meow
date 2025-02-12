9c35a3c897045e3957f6de97ff8c3a8e1f97ca09
BeansDed <118106859+BeansDed@users.noreply.github.com>
2025-02-12 18:07:01 +0800

    finish

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const music = document.getElementById("background-music");
+
+    // Play background music automatically
+    music.volume = 0.5;
+    music.play();
+});
